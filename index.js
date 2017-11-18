76fa75283aa9f5e7ffc795fa33a649bdff163ce9
Shana A <shana.arias@ucf.edu>
2017-11-18 05:16:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Shana';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+let shortGreeting = 'Welcome, ' + currentUser.slice(0,1) + '!';
